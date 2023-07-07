7f72e42b4f59980bd760cc199604fa578ac581f5
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-07 13:26:29 +0530

    nested div

--- Inception/App.js
+++ Inception/App.js
@@ -5,22 +5,28 @@
  *          <h1>I'm h1 tag</h1>
  *          <h2>I'm h2 tag</h2>
  *      </div>
+ *      <div id="child2">
+ *          <h1>I'm h1 tag</h1>
+ *          <h2>I'm h2 tag</h2>
+ *      </div>
  * </div>
  * 
  * ReactElement(Object) => HTML(Browser Understands)
  * 
  */
 
-const parent = React.createElement(
-    "div", 
-    {id: "parent"},
-    React.createElement(
-        "div", 
-        {id: "child"}, [
-            React.createElement("h1", {}, "I'm h1 tag"), 
-            React.createElement("h2", {}, "I'm h2 tag")
-        ]
-    )
+const parent = React.createElement("div", {id: "parent"},[
+        React.createElement("div", {id: "child"}, [
+                React.createElement("h1", {}, "I'm h1 tag"), 
+                React.createElement("h2", {}, "I'm h2 tag")
+            ]
+        ),
+        React.createElement("div", {id: "child2"}, [
+                React.createElement("h1", {}, "I'm h1 tag"), 
+                React.createElement("h2", {}, "I'm h2 tag")
+            ]
+        )
+    ]
 );
 
 console.log('parent', parent);
