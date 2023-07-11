ef6e53cea9d0106de73d4a97d499aefccba88610
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-11 14:29:19 +0530

    using function inplace of arrow fn

--- layingTheFoundation/App.js
+++ layingTheFoundation/App.js
@@ -2,19 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React Element
-const jsxHeading = (
+// const jsxHeading = (
+//   <h1 className="head" tabIndex="5">
+//     Namaste Javascript using JSX
+//   </h1>
+// );
+
+// React Component
+const Title = function () { 
+    return (
     <h1 className="head" tabIndex="5">
         Namaste Javascript using JSX
     </h1>
-);
+    );
+}
 
-// React Component
 const HeadingComponent = () => (
-    <div id="container">
-        <h1 className="heading">Namaste React Functional Component</h1>;
-    </div>
+  <div id="container">
+    <Title />
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(jsxHeading);
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
