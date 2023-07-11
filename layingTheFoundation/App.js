99ea5ab889a9cdb262f5100ba227819421aea07f
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-11 12:41:50 +0530

    JSX transpilation babel

--- layingTheFoundation/App.js
+++ layingTheFoundation/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => Object => HTMLElement(render)
+// React.createElement => React Element - JS Object => HTMLElement(render)
 
-const heading = React.createElement("h1", {id: "heading"}, "Namaste React");
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
 
-console.log('heading', heading);
+console.log("heading", heading);
 
 // JSX - HTML - like or XML - like Syntax
-const jsxHeading =  <h1 id="heading">Namaste Javascript using JSX</h1>
+// JSX (transpiled before it reaches the JS) - PARCEL - Babel (JS Complier)
+// JSX => Babel transpiles it to React.createElement => React Element - JS Object => HTMLElement(render)
+const jsxHeading = (
+    <h1 className="head" tabIndex="5">
+        Namaste Javascript using JSX
+    </h1>
+);
 
-console.log('jsxheading', jsxHeading);
+console.log("jsxheading", jsxHeading);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
