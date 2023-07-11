8683450ff6184a77ba1c77bc28d69f25e94c6ac3
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-11 19:53:16 +0530

    resturant app base setup

---
+++ talkIsCheap_showMeTheCode/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+/**
+ * 
+ * Header
+ * - Logo
+ * - Nav Items
+ * Body
+ * - Search
+ * - RestaurantContainer
+ *  - RestaurantCard
+ * Footer
+ * - Links
+ * - Address
+ * - Contact
+ * 
+ */
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(jsxHeading);
+root.render(<HeadingComponent />);
