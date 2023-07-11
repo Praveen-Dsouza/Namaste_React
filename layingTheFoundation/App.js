88fdcb96442216d56e71db089a3d55117953b2a3
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-11 13:00:40 +0530

    react functional component

--- layingTheFoundation/App.js
+++ layingTheFoundation/App.js
@@ -1,22 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React.createElement => React Element - JS Object => HTMLElement(render)
-
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
-
-console.log("heading", heading);
-
-// JSX - HTML - like or XML - like Syntax
-// JSX (transpiled before it reaches the JS) - PARCEL - Babel (JS Complier)
-// JSX => Babel transpiles it to React.createElement => React Element - JS Object => HTMLElement(render)
+// React Element
 const jsxHeading = (
     <h1 className="head" tabIndex="5">
         Namaste Javascript using JSX
     </h1>
 );
 
-console.log("jsxheading", jsxHeading);
+// React Component
+const HeadingComponent = () => (
+    <div id="container">
+        <h1 className="heading">Namaste React Functional Component</h1>;
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
