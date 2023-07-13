e5395582371b3c92d10a99eb2f66f0ecfa29c48d
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-13 12:23:21 +0530

    search functionality added

--- E05_letsGetHooked/src/components/Body.js
+++ E05_letsGetHooked/src/components/Body.js
@@ -6,6 +6,7 @@ const Body = () => {
 
     // State Variable - Super powerful variable
     const [listOfRestuarants, setListOfRestuarants] = useState(resList);
+    const [query, setQuery] = useState("");
 
     // Normal JS Variable                State Variable
     // let listOfRestuarants = [] equals setListOfRestuarants([])
@@ -20,10 +21,22 @@ const Body = () => {
                     setListOfRestuarants(filteredList)
                 }}>Top Rated Restaurants</button>
             </div>
+            <div className="search">
+                <input type="text" placeholder="search your resturant" onChange={(e) => setQuery(e.target.value)} />
+            </div>
+            
             <div className="res-container">
-                {listOfRestuarants.map((restaurant) => (
-                    <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
-                ))}
+                {listOfRestuarants.filter(res => {
+                        if (query === '') {
+                            return listOfRestuarants;
+                        } else if (res.data.name.toLowerCase().includes(query.toLowerCase())) {
+                            return listOfRestuarants;
+                        }
+                    })
+                    .map((restaurant) => (
+                        <RestaurantCard key={restaurant.data.id} resData={restaurant}/>
+                    ))
+                }
             </div>
         </div>
     )
