40c899f9b32376bdb2a530810058703c887323b5
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-15 17:34:47 +0530

    set the state variables

--- E08_letsGetClassy/src/components/User.js
+++ E08_letsGetClassy/src/components/User.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
+
 const User = ({name}) => {
+    const [count] = useState(0);
+    const [count2] = useState(1);
 
     return (
         <div className="user-card">
+            <h1>Count = {count}</h1>
+            <h1>Count2 = {count2}</h1>
             <h2>Name: {name}</h2>
             <h2>Location: Mumbai</h2>
             <h2>Contact: dsouzapraveen24</h2>
--- E08_letsGetClassy/src/components/UserClass.js
+++ E08_letsGetClassy/src/components/UserClass.js
@@ -3,13 +3,21 @@ import React from "react"
 class UserClass extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            count: 0,
+            count2: 1
+        }
     }
 
     render() {
         const { name, location } = this.props
+        const { count, count2 } = this.state
 
         return (
             <div className="user-card">
+                <h1>Count = {count}</h1>
+                <h1>Count2 = {count2}</h1>
                 <h2>Name: {name}</h2>
                 <h2>Location: {location}</h2>
                 <h2>Contact: dsouzapraveen24</h2>
