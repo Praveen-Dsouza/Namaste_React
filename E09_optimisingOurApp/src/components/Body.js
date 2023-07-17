6745eac2883ff211121637f3b368cbae4245ad9c
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-17 19:29:34 +0530

    onlinestatus hook created and added in header

--- E09_optimisingOurApp/src/components/Body.js
+++ E09_optimisingOurApp/src/components/Body.js
@@ -2,6 +2,7 @@ import RestaurantCard from "./RestaurantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
 
@@ -24,6 +25,12 @@ const Body = () => {
         const restaurantData = json?.data?.cards[2]?.data?.data?.cards
         setListOfRestuarants(restaurantData)
         setFilteredRestaurant(restaurantData)
+    }
+
+    const onlineStatus = useOnlineStatus();
+
+    if (onlineStatus === false) {
+        return <h1>Looks like you're offline!! Please check your internet connection.</h1>
     }
 
     return listOfRestuarants.length === 0 ? <Shimmer/>: (
--- E09_optimisingOurApp/src/components/Header.js
+++ E09_optimisingOurApp/src/components/Header.js
@@ -1,9 +1,11 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
     const [login, setLogin] = useState("Login");
+    const onlineStatus = useOnlineStatus();
 
     return (
         <div className="header">
@@ -12,6 +14,7 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
+                    <li>Online Status: {onlineStatus? "🟢": "🔴"}</li>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About Us</Link></li>
                     <li><Link to="/contact">Contact Us</Link></li>
