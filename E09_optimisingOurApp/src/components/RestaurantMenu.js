7114e7853ef07b6cce03df29598ec38d46c4a4dc
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-17 16:07:36 +0530

    base setup

---
+++ E09_optimisingOurApp/src/components/RestaurantMenu.js
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_URL } from "../utils/constants";
+
+const ResturantMenu = () => {
+  const [resInfo, setResInfo] = useState(null);
+  const { resId } = useParams();
+
+  useEffect(() => {
+    fetchMenu();
+  }, []);
+
+  const fetchMenu = async () => {
+    const data = await fetch(MENU_URL + resId); // JIO : 463943, Rangoon 106763
+    const json = await data.json();
+    console.log("menu", json);
+    setResInfo(json.data);
+  };
+
+  if (resInfo === null) return <Shimmer />;
+
+  const { name, cuisines, costForTwoMessage } =
+    resInfo?.cards[0]?.card?.card?.info;
+
+  const { itemCards } =
+    resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
+  console.log("card", itemCards);
+
+  return (
+    <div className="menu">
+      <h1>{name}</h1>
+      <p>
+        {cuisines.join(", ")} - {costForTwoMessage}
+      </p>
+      <h1>Menu</h1>
+      <ul>
+        {itemCards?.map((item) => (
+          <li key={item?.card?.info?.id}>
+            {item?.card?.info?.name} - {"Rs."} {item?.card?.info?.price / 100 }
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ResturantMenu;
