11871510c8873d9dc45012fafad8a01d107a817e
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-17 20:21:48 +0530

    added lazy loading

--- E09_optimisingOurApp/src/App.js
+++ E09_optimisingOurApp/src/App.js
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import ResturantMenu from "./components/RestaurantMenu";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
+const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"));
 
 const AppLayout = () => {
     return (<div className="app">
@@ -26,11 +28,15 @@ const appRouter = createBrowserRouter([
             },
             {
                 path: "/about",
-                element: <About/>
+                element: (<Suspense fallback={<h1>Loading...</h1>}><About/></Suspense>)
             }, 
             {
                 path: "/contact",
                 element: <Contact/>
+            },
+            {
+                path: "/grocery",
+                element: (<Suspense fallback={<h1>Loading...</h1>}><Grocery/></Suspense>)
             },
             {
                 path: "/restaurants/:resId",
--- E09_optimisingOurApp/src/components/Header.js
+++ E09_optimisingOurApp/src/components/Header.js
@@ -18,6 +18,7 @@ const Header = () => {
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/about">About Us</Link></li>
                     <li><Link to="/contact">Contact Us</Link></li>
+                    <li><Link to="/grocery">Grocery</Link></li>
                     <li><Link to="/cart">Cart</Link></li>
                     <button 
                         className="login-btn" 
