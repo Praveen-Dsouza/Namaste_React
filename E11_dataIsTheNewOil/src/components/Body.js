07c60f294a6cf9c4105a90ca30588d943820c30d
Praveen Dsouza <dsouzapraveen24@gmail.com>
2023-07-18 19:03:39 +0530

    added accordian

--- E11_dataIsTheNewOil/src/components/Body.js
+++ E11_dataIsTheNewOil/src/components/Body.js
@@ -13,7 +13,7 @@ const Body = () => {
   const RestaurantCardPromoted = withPromotedLabel(RestaurantCard);
 
   // Whenever state variables update, react triggers a reconciliation cycle (re-renders the component)
-  console.log("Body Rendered");
+  // console.log("Body Rendered");
 
   useEffect(() => {
     fetchData();
--- E11_dataIsTheNewOil/src/components/RestaurantMenu.js
+++ E11_dataIsTheNewOil/src/components/RestaurantMenu.js
@@ -1,4 +1,5 @@
 import useRestaurantMenu from "../utils/useRestaurantMenu";
+import RestaurantCategory from "./RestaurantCategory";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
 
@@ -14,22 +15,18 @@ const ResturantMenu = () => {
 
   const { itemCards } =
     resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[1]?.card?.card;
-  console.log("card", itemCards);
+
+  const categories = resInfo?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards?.filter((c) => 
+    c.card?.card?.["@type"] === "type.googleapis.com/swiggy.presentation.food.v2.ItemCategory"
+  );
 
   return (
-    <div className="menu">
-      <h1>{name}</h1>
-      <p>
+    <div className="text-center">
+      <h1 className="font-bold my-6 text-2xl">{name}</h1>
+      <p className="font-bold text-lg">
         {cuisines.join(", ")} - {costForTwoMessage}
       </p>
-      <h1>Menu</h1>
-      <ul>
-        {itemCards?.map((item) => (
-          <li key={item?.card?.info?.id}>
-            {item?.card?.info?.name} - {"Rs."} {item?.card?.info?.price / 100 }
-          </li>
-        ))}
-      </ul>
+      {categories?.map((category) => <RestaurantCategory key={category?.card?.card?.title} data={category?.card?.card}/>)}
     </div>
   );
 };
